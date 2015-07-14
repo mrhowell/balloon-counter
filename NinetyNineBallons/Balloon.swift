6eb1dbfc34331d81e07d3caa4c6f227248c89a45
Arikko Howell <arikkoh@gmail.com>
2015-07-13 19:53:18 -0700

    second commit

--- NinetyNineBallons/Balloon.swift
+++ NinetyNineBallons/Balloon.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+import UIKit
+
+struct Balloon {
+    
+    var number: Int = 0
+    var image = UIImage(named: <#String#>)
+    
+}
+
+func createBalloons () {
+
+    var myBalloon = Balloon()
+    var balloonCollection = [0...99]
+
+    
+    let randomImageNumber = Int(arc4random_uniform(UInt32(4)))
+
+    
+    var x = randomImageNumber
+        
+        switch x {
+        case 1:
+            myBalloon.image = "RedBalloon1.jpg"
+        case 2:
+            myBalloons.image = "RedBalloon2.jpg"
+        case 3:
+            myBalloons.image = "RedBalloon3.jpg"
+        case 4:
+            myBalloons.image = "RedBalloon4.jpg"
+        
+        
+        i++
+            
+    }
+    
+    
+}
--- NinetyNineBallons/ViewController.swift
+++ NinetyNineBallons/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var balloonImage: UIImageView!
+    @IBOutlet weak var balloonCount: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var startText = "No Balloons"
+    
+        balloonCount.text = startText
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func barNextButtonPressed(sender: UIBarButtonItem) {
+    }
 
 }
 
